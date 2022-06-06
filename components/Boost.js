a1c58d56ef72f8dd9915d53dd67c5a39d52fe5be
Dhaval Kolhe <72658475+dhavalkolhe@users.noreply.github.com>
2022-06-06 15:44:49 +0530

    Bug fix

--- components/Boost.js
+++ components/Boost.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Image from "next/image";
 
-import meteor from "../assets/Meteor.svg";
+import meteor from "../assets/meteor.svg";
 
 const Title = styled.p`
   font-family: "Poppins", sans-serif;
